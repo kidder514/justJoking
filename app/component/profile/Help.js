8357729aba83e760a3f9b7c7d6e705de118d2ea1
Duo Lu <kidder5142@gmail.com>
2018-05-12 22:30:19 +1000

    add help page

--- app/component/profile/Help.js
+++ app/component/profile/Help.js
@@ -1,17 +1,29 @@
 import React from 'react';
-import { StyleSheet, ScrollView } from 'react-native';
-import { whiteColor} from '../../asset/style/common';
-import Markdown from 'react-native-simple-markdown'
+import { StyleSheet, ScrollView, Text } from 'react-native';
+import { whiteColor } from '../../asset/style/common';
 
 class Help extends React.PureComponent {
-	
+
 	render() {
 		return (
 			<ScrollView style={style.container}>
-				<Markdown styles={markdownStyles}>
-					
-				</Markdown>
-			</ScrollView> 
+				<Text>
+					From the JustJokes developer:
+				</Text>
+				<Text style={style.linePadding}>
+					Welcome to the world of funny things, the purpose of this app is to provide a place for you to relax after a long day of work, study or whatever you do.
+					I will be really thankful if you can help me build and grow this community by posting more funny things or asking your friends to download this app. 
+				</Text>
+				<Text style={style.linePadding}>
+					If will be even better if you can click some ads and that will help me cover the cost of my server.
+				</Text>
+				<Text style={style.linePadding}>
+					You can join our facebook group https://www.facebook.com/groups/215145849077262/ to share you thought about this app, 
+				</Text>
+				<Text>
+					or contact me on justjokesapp@gmail.com, I will try my best to get back to you ASAP.
+				</Text>
+			</ScrollView>
 		)
 	}
 }
@@ -20,26 +32,10 @@ const style = StyleSheet.create({
 	container: {
 		backgroundColor: whiteColor,
 		padding: 20,
+	},
+	linePadding: {
+		paddingTop: 10
 	}
 });
-
-const markdownStyles = {
-	heading1: {
-		fontSize: 24,
-		paddingTop: 8,
-		paddingBottom: 8,
-		color: 'purple',
-	  },
-	  link: {
-		color: 'pink',
-	  },
-	  mailTo: {
-		color: 'orange',
-	  },
-	  text: {
-		color: '#555555',
-	  },
-}
-
 
 export default Help;
--- app/component/profile/TermCondition.js
+++ app/component/profile/TermCondition.js
@@ -1,32 +1,53 @@
 import React from 'react';
-import { StyleSheet, ScrollView } from 'react-native';
-import { whiteColor} from '../../asset/style/common';
-import Markdown from 'react-native-simple-markdown'
+import { StyleSheet, ScrollView, Text } from 'react-native';
+import { whiteColor } from '../../asset/style/common';
 
 class TermCondition extends React.PureComponent {
-	
+
 	render() {
 		return (
 			<ScrollView style={style.container}>
-				<Markdown styles={markdownStyles}>
-					There is no such thing as terms and conditions here, I hope you all enjoy this app and find it as a good entertainment after a long day of work or study. If you like this app and are willing to share, upload some funny images or jokes to bring this joke community to another level. {'\n\n'}
+				<Text>
+					There is no such thing as terms and conditions here, I hope you all enjoy this app and find it as a good entertainment after a long day of work or study. If you like this app and are willing to share, upload some funny images or jokes to bring this joke community to another level.
+				</Text>
 
-					However, there are something that are not allowed here in compliance with the rule of app store. Please follow the following rule otherwise Google might shut down my app. {'\n\n'}
-					- No sexually explicit content {'\n'}
-					- No hate speech{'\n'}
-					- No violence{'\n'}
-					- No sensitive events{'\n'}
-					- No bullying or harassment{'\n'}
-					- No financial instruments{'\n'}
-					- No gambling on the app{'\n'}
-					- No illegal activities{'\n'}
-					- No other inappropriate content{'\n\n\n'}
+				<Text style={style.linePadding}>
+					However, there are something that are not allowed here in compliance with the rule of app store. Please follow the following rule otherwise Google might shut down my app.
+				</Text>
 
+				<Text style={style.linePadding}>
+					- No sexually explicit content 			
+				</Text>
+				<Text>
+					- No hate speech
+				</Text>
+				<Text>
+					- No violence		
+				</Text>
+				<Text>
+					- No sensitive events
+				</Text>
+				<Text>
+					- No bullying or harassment
+				</Text>
+				<Text>
+					- No financial instruments
+				</Text>
+				<Text>
+					- No gambling on the app
+				</Text>
+				<Text>
+					- No illegal activities
+				</Text>
+				<Text>
+					- No other inappropriate content
+				</Text>
+
+				<Text style={style.linePadding}>
 					Again, thank you for using my app, and hope you can LYAO. 
-
-				</Markdown>
-			</ScrollView> 
-		)
+				</Text>
+			</ScrollView>
+		);
 	}
 }
 
@@ -34,26 +55,10 @@ const style = StyleSheet.create({
 	container: {
 		backgroundColor: whiteColor,
 		padding: 20,
+	},
+	linePadding: {
+		paddingTop: 10
 	}
 });
-
-const markdownStyles = {
-	heading1: {
-		fontSize: 24,
-		paddingTop: 8,
-		paddingBottom: 8,
-		color: 'purple',
-	  },
-	  link: {
-		color: 'pink',
-	  },
-	  mailTo: {
-		color: 'orange',
-	  },
-	  text: {
-		color: '#555555',
-	  },
-}
-
 
 export default TermCondition;
